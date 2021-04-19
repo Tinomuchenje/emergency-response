ea51272556e004992b97e766595451d4701da5af
TinoJoel Muchenje <tinojoel90@gmail.com>
2021-04-19 23:19:44 +0200

    Calling from app functionality
    - Added plugin to allow calling within application
    - Added method to make a call from passed in number

--- src/app/emergency-services/emergency-services.component.ts
+++ src/app/emergency-services/emergency-services.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 @Component({
-  selector: 'emergency-services',
+  selector: 'app-emergency-services',
   templateUrl: './emergency-services.component.html',
   styleUrls: ['./emergency-services.component.scss'],
 })
 export class EmergencyServicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private callNumber: CallNumber) { }
 
   ngOnInit() {}
+
+  callNow(numberToCall: string) {
+    this.callNumber.callNumber(numberToCall, true)
+      .then(result => console.log('Launched dialer!', result))
+      .catch(error => console.log('Error launching dialer', error));
+  }
 
 }
