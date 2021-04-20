4af725a6cc6c376b5c218442e0c15f0b5e94fbea
TinoJoel Muchenje <tinojoel90@gmail.com>
2021-04-20 09:44:59 +0200

    UserLocation and Android build
    - Executed android build
    - Added method to get current user location

--- src/app/emergency-services/emergency-services.component.ts
+++ src/app/emergency-services/emergency-services.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CallNumber } from '@ionic-native/call-number/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-emergency-services',
@@ -8,14 +9,30 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
 })
 export class EmergencyServicesComponent implements OnInit {
 
-  constructor(private callNumber: CallNumber) { }
+  longitude;
+  latitude;
+  constructor(private callNumber: CallNumber, private geolocation: Geolocation) { }
 
   ngOnInit() {}
 
   callNow(numberToCall: string) {
+
+    this.getUserLocation();
+
     this.callNumber.callNumber(numberToCall, true)
       .then(result => console.log('Launched dialer!', result))
       .catch(error => console.log('Error launching dialer', error));
   }
 
+  getUserLocation(){
+    this.geolocation.getCurrentPosition().then((response) => {
+      this.latitude = response.coords.latitude;
+      this.longitude = response.coords.longitude;
+      console.log('latitude',this.latitude);
+      console.log('longitude',this.longitude);
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+
+  }
 }
--- src/app/emergency-services/emergency-services.module.ts
+++ src/app/emergency-services/emergency-services.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { EmergencyServicesComponent } from './emergency-services.component';
 
 @NgModule({
   imports: [ CommonModule, FormsModule, IonicModule],
   declarations: [EmergencyServicesComponent],
-  exports: [EmergencyServicesComponent]
+  exports: [EmergencyServicesComponent],
+  providers: [Geolocation]
 })
 export class EmergencyServicesComponentModule {}
